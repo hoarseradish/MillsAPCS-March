c0156021563712c4dacd2a7b50af5fa44509f61c
hoarseradish <trex_gavin@yahoo.com>
2016-03-21 08:53:34 -0700

    3/21
    3/21

---
+++ ElevensStudentStarterCode/Activity1 Starter Code/CardTester.java
@@ -0,0 +1,15 @@
+/**
+ * This is a class that tests the Card class.
+ */
+public class CardTester {
+
+	/**
+	 * The main method in this class checks the Card operations for consistency.
+	 *	@param args is not used.
+	 */
+	public static void main(String[] args) {
+		Card one = new Card( "six ", "heart", 6);
+		System.out.println(one.toString());
+		
+	}
+}
---
+++ ElevensStudentStarterCode/Activity2  Starter Code/DeckTester.java
@@ -0,0 +1,23 @@
+/**
+ * This is a class that tests the Deck class.
+ */
+public class DeckTester {
+
+    /**
+     * The main method in this class checks the Deck operations for consistency.
+     *	@param args is not used.
+     */
+    public static void main(String[] args) {
+        String[] ranks = {"jack", "queen", "king"}
+        String[] suits = {"blue","red"};
+        int [] pointValues = {11, 12, 13};
+        Deck d = new Deck(ranks, suits, pointValues);
+        System.out.println(d.toString());
+        for (int i =0; i<d.size(); i++) {
+            Card a = d.deal();
+            
+            System.out.println(a.toString());
+        }
+    }
+}
+
